eff4a11e86d504a5495ce0ce80c069ca95c72b7d
AryanKulkarni11042005 <aryan.kulkarni@vit.edu.in>
2026-05-01 19:30:59 +0530

    Using openai/gpt-oss-120b

--- llm.py
+++ llm.py
@@ -7,5 +7,5 @@ def create_llm():
     groq_api_key = os.getenv("GROQ_API_KEY")
     llm = ChatGroq(
         api_key=groq_api_key,
-        model_name="llama-3.1-8b-instant")
+        model_name="openai/gpt-oss-120b")
     return llm
